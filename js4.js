6cc15e6079c6560b2124eab023498d7990b9f7d6
MICHENUY <adriytkachuk162@gmail.com>
2022-11-28 20:33:07 +0200

    Initial commit

---
+++ js4.js
@@ -0,0 +1,41 @@
+
+let users = [
+    {name: 'petya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'max', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'oleh', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (let i = 0; i < users.length; i++){
+
+    document.write(`<div>
+<h2>${users.name} </h2>
+<p>${users.age}</p>
+<h5>${users.status}</h5>
+</div>`);
+}
+
+let sh = [
+    { np :'erge'},
+    { np:'erhh'},
+    { np:'rthe'},
+    { np:'ertgh'},
+    { np:'ytik'},
+    { np:'srvg'},
+    { np :'trryt'},
+    { np :'grrd'},
+    { np:'sgh'},
+    { np:'sgdsv'},
+
+];
+for (let i = 0; i < sh.length; i++) {
+    document.write(`<div>
+<p>${sh.np}</p>
+</div>`)
+}
