b90d23223cf6d8f4e204050820dbf0616f364bc7
MICHENUY <adriytkachuk162@gmail.com>
2022-12-19 15:54:50 +0200

    ---

--- js4.js
+++ js4.js
@@ -1,41 +1,132 @@
 
-let users = [
-    {name: 'petya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'max', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'oleh', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-for (let i = 0; i < users.length; i++){
 
-    document.write(`<div>
-<h2>${users.name} </h2>
-<p>${users.age}</p>
-<h5>${users.status}</h5>
-</div>`);
-}
 
-let sh = [
-    { np :'erge'},
-    { np:'erhh'},
-    { np:'rthe'},
-    { np:'ertgh'},
-    { np:'ytik'},
-    { np:'srvg'},
-    { np :'trryt'},
-    { np :'grrd'},
-    { np:'sgh'},
-    { np:'sgdsv'},
+// let sh = [
+//     { xxx:'erge'},
+//     { xxx:'erhh'},
+//     { xxx:'rthe'},
+//     { xxx:'ertgh'},
+//     { xxx:'ytik'},
+//     { xxx:'srvg'},
+//     { xxx:'trryt'},
+//     { xxx:'grrd'},
+//     { xxx:'sgh'},
+//     { xxx:'sgdsv'},
+//
+// ];
+//
+// for (let i = 0; i < sh.length; i++) {
+//     const she = sh[i];
+//     document.write(`
+// <div>
+// <p>
+// ${she.xxx}
+// </p>
+// </div>
+// `)
+//
+// }
 
-];
-for (let i = 0; i < sh.length; i++) {
-    document.write(`<div>
-<p>${sh.np}</p>
-</div>`)
-}
+// let users = [
+//     {name: 'petya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'max', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'oleh', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// for (let i = 0; users.length - 2; i++){
+//     const user = users[i]
+//     document.write(`<div>
+// <h2>${user.name} </h2>
+// <p>${user.age}</p>
+// <h5>${user.status}</h5>
+// </div>`);
+// }
+
+
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// for (const listOfItem of listOfItems) {
+//     document.write(`
+//  <ul>
+//  <li>
+//  ${listOfItem}
+// </li>
+// </ul> `)
+// }
+
+
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (const product of products) {
+//     document.write(
+//         `<div class="product_card">
+// <h3 class="product_title">${product.title} - ${product.price} UAN</h3>
+// <img src="${product.image}" alt="staff" class="product-image">
+// </div>`
+//     )
+// }
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//     const user = users[i];
+//     if (user.status > false ){
+//         document.write(`<h1>${user.name} ${user.age} ${user.status}</h1>`)
+//     }
+//
+// }
+// for (let i = 0; i < users.length; i++) {
+//     const user = users[i];
+//     if (user.status < true ){
+//         document.write(`<h1>${user.name} ${user.age} ${user.status}</h1>`)
+//     }
+//
+// }
+
+// for (let i = 0; i < users.length; i++) {
+//     const user = users[i];
+//     if (user.age > 30 ){
+//         document.write(`<h1>${user.name} ${user.age} ${user.status}</h1>`)
+//     }
+//
+// }
+
